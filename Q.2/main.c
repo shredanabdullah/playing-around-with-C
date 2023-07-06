ede170cb755b8f15bcb65a094d41ffa0a0483f80
Shredan Abdullah <105118008+shredanabdullah@users.noreply.github.com>
2023-07-06 16:04:41 +0300

    Add files via upload

---
+++ Q.2/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+(2) Write a program that print your name and your grade in a new line.
+*/
+int main()
+{
+    printf("Shredan Abdullah\n");
+    printf("gpa: 3\n");
+    return 0;
+}

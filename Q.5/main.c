16edbf268fcc8c36449a852b432ee83e47ebff62
Shredan Abdullah <105118008+shredanabdullah@users.noreply.github.com>
2023-07-06 16:22:35 +0300

    Add files via upload

---
+++ Q.5/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+(5) Write a program to print the ASCII value of a character input by the user.
+*/
+int main()
+{
+    printf("enter character!\n");
+    char x;
+    scanf("%c",&x);
+    printf("the ASCII value of a character is %d",x);
+
+    return 0;
+}

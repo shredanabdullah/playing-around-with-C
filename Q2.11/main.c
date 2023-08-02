f06270aee8e03c2e4adbd4509f7dfe7a27e9aeff
Shredan Abdullah <105118008+shredanabdullah@users.noreply.github.com>
2023-08-03 01:26:23 +0300

    Update main.c

--- Q2.11/main.c
+++ Q2.11/main.c
@@ -3,18 +3,19 @@
 /*(11) Write a C function that returns 1 if the input number is a power of 2
 and return 0 if the input number is not power of 2.
 For example: 1, 2 and 16 are power of 2. 0, 10 and 30 are not power of 2.*/
-int isPower(int x);
+void isPower(int x);
 int main()
 {
-    int x;
-    printf("enter the number: ");
-    scanf("%d",&x);
-    int result=isPower(x);
-    printf("%d\n",result);
+    isPower(350,7);
     return 0;
 }
-int isPower(int num){
- if((num/2)%2==0||num==2){
-    return 1;
- }else return 0;
+void isPower(int x){
+    int reminder;
+while(x!=1){
+    reminder=x%2;
+    x=x/2;
+}
+ if(reminder==0)printf("is Power");
+ else printf("is not Power");
+
 }

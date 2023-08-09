124184c12114a10ec14c20d76d4e8700abc03629
Shredan Abdullah <105118008+shredanabdullah@users.noreply.github.com>
2023-08-09 18:02:16 +0300

    Update main.c

--- Q2.11/main.c
+++ Q2.11/main.c
@@ -6,7 +6,7 @@ For example: 1, 2 and 16 are power of 2. 0, 10 and 30 are not power of 2.*/
 void isPower(int x);
 int main()
 {
-    isPower(16);
+    isPower(8);
     return 0;
 }
 void isPower(int x){
